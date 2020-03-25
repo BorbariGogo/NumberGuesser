ff671d154f3c1868ed32f93b5c269f6e1fbba144
BorbariGogo <borbarigogo@gmail.com>
2020-03-25 14:54:55 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point
+        static void Main(string[] args)
+        {
+            string name = "Blessing";
+            int you = 1;
+            Console.WriteLine("{0} is {1}",name,you);
+        }
+    }
+}
